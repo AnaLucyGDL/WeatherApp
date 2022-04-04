bef19a0b7a37a123eb6813baf6a0fbdb0f9c01c1
Bety Martinez <trism460@gmail.com>
2022-04-03 20:11:24 -0500

    Add openWeather variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: ''
+  openWeather: {
+    key: '2c6706b6473943612b6c95e58ffdd0fb',
+    url: 'https://api.openweathermap.org/data/2.5/'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,10 @@
 
 export const environment = {
   production: false,
-  apiUrl: ''
+  openWeather: {
+    key: '2c6706b6473943612b6c95e58ffdd0fb',
+    url: 'https://api.openweathermap.org/data/2.5/'
+  }
 };
 
 /*
