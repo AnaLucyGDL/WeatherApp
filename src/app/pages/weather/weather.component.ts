cc0f00caf57038619a814ce857a990f358c7ed64
unknown <analucygdl@gmail.com>
2022-04-03 21:02:32 -0500

    API working

--- src/app/pages/weather/weather.component.ts
+++ src/app/pages/weather/weather.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { WeatherData } from '@app/shared/interface/weather.interface';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.scss']
+  styleUrls: ['./weather.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class WeatherComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class WeatherComponent {
+  @Input() public weather!:WeatherData;
+  public BASE_URL = 'http://openweathermap.org/img/wn'
 }
