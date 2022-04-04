22c1773895792d4c652d0a41d34b2290cf462cc0
Bety Martinez <trism460@gmail.com>
2022-04-03 20:42:23 -0500

    Add input

--- src/app/pages/weather/weather.component.ts
+++ src/app/pages/weather/weather.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { WeatherData } from '@app/shared/interfaces/weather.interface';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.scss']
+  styleUrls: ['./weather.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class WeatherComponent implements OnInit {
-
+export class WeatherComponent {
+  @Input() public weather!: WeatherData;
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
